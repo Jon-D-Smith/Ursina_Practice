f203b7c174558b1bc2d04dced5dbf2982b750301
Jon Smith <aardvarkjon94@yahoo.com>
2021-11-22 22:43:51 -0600

    Added basic camera follow

--- main.py
+++ main.py
@@ -2,16 +2,31 @@ from ursina import *
 
 app = Ursina()
 
+window.title = 'Test game'                
+window.borderless = False               
+window.fullscreen = True              
+window.exit_button.visible = True     
+window.fps_counter.enabled = True 
+
 player = Entity(model="cube", color= color.red, scale_y = 2)
+floor = Entity(model='quad', color=color.orange, position=(0,0,1), scale=1.5, rotation=(0,0,45), texture='brick')
 
 def update():
-    player.x += held_keys['d'] * .1
-    player.x -= held_keys['a'] * .1
-    player.y += held_keys['w'] * .1
-    player.y -= held_keys['s'] * .1
-    player.z += held_keys['q'] * .1
-    player.z -= held_keys['e'] * .1
+    player.x += held_keys['d'] * (5 * time.dt)
+    player.x -= held_keys['a'] * (5 * time.dt)
+    player.y += held_keys['w'] * (5 * time.dt)
+    player.y -= held_keys['s'] * (5 * time.dt)
+    player.z += held_keys['q'] * (5 * time.dt)
+    player.z -= held_keys['e'] * (5 * time.dt)
+    camera.position = (player.x, player.y +2 ,player.z - 10) 
+
     
+def input(key):
+    if key == 'space':
+        player.y += 3
+        invoke(setattr, player, 'y', player.y-3, delay=.2)
+            
+            
 
 
 app.run()
